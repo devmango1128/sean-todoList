8c58659fda15805ad25ee1c5836eea8a16330f25
devmango1128 <devmango1128@gmail.com>
2024-03-13 18:22:15 +0900

    component

---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,54 @@
+<template>
+    <form @submit.prevent = "todoAdd">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input :type="type" class="form-control " v-model="todo" placeholder="할일을 입력하세요.">
+        </div>
+        <div>
+          <button class="btn btn-primary btn-success" type="submit">등록</button>
+        </div>
+      </div>
+      <div v-show="hasError" class="text-danger mt-2">
+        할일을 입력해주세요.
+      </div>
+    </form>
+</template>
+<script>
+
+import { ref } from "vue";
+
+export default {
+
+    setup(props, context) {
+
+        const todo = ref('')
+        const hasError = ref(false)
+
+        //todo 추가
+        const todoAdd = () => {
+            if(todo.value === '') {
+                hasError.value = true
+            } else {
+                context.emit('add-todo', {
+                    id : Date.now(),
+                    subject : todo.value,
+                    completed: false 
+                })
+                todo.value = ''
+                hasError.value = false
+            }
+        }
+
+        return {
+            todo,
+            hasError,
+            todoAdd
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
