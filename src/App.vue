289a389f476c6aacd05e1ae5b03d628f013670d3
devmango1128 <devmango1128@gmail.com>
2024-03-17 23:12:22 +0900

    todo delete

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <div v-if="!todos.length" class="mt-2">
       등록된 할일이 없습니다!
     </div>
-    <TodoList :todos="todos" @toggle-todo="toggleTodo"/>
+    <TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="todoDelete"/>
   </div>
 </template>
 
@@ -26,8 +26,10 @@ export default {
     const todos = ref([])
 
     //todo 삭제
-    const todoDelete = (index) => {
-      todos.value.splice(index, 1)
+    const todoDelete = (id) => {
+      //삭제할 데이터의 id로 index를 구해온다.
+      const todoDeleteIndex = todos.value.findIndex((todo) => todo.id === id)
+      todos.value.splice(todoDeleteIndex, 1)
     }
 
     //자식 컴포넌트에서 데이터 받아오기(TodoSimpleForm.vue)
@@ -38,6 +40,7 @@ export default {
 
     //체크박스 선택 시 할일 완료/미완료 
     const toggleTodo = (id) => {
+      //id로 할일/미완료 할 데이터의 정보를 들고온다.
       const todoCompleted = todos.value.find((todo) => todo.id === id)
       todoCompleted.completed = !todoCompleted.complete
     }
