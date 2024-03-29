9ce1f72f7bb877c33a45aebceeda2c7111925a65
devmango1128 <devmango1128@gmail.com>
2024-03-29 23:38:06 +0900

    Toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="alert alert-success toast-box" :class="`alert-${type}`" :type ="ToastAlertType" role="alert">
+        {{ message }}
+    </div>
+</template>
+
+<script setup>
+    // props는 객체 형태로 선언되어야 합니다.
+    defineProps({
+        message : {
+            type : String,
+            required : true
+        },
+        type : {
+            type : String,
+            default: 'success'
+        }
+            
+    });
+</script>
+<style>
+    .toast-box {
+        position: fixed;
+        top: 10px;
+        right: 10px;
+    }
+</style>
